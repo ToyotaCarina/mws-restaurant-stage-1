d316445a7c5329ff7d564ebafc3fe187f47d1a00
Karina Lund <toyotac7@gmail.com>
2018-06-19 18:51:17 +0200

    ServiceWorker offline mode

--- js/main.js
+++ js/main.js
@@ -179,3 +179,16 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+/**
+ * Service Worker
+ */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+}
---
+++ sw.js
@@ -0,0 +1,69 @@
+var staticCacheName = 'restaurant-static-v1';
+var contentImgsCache = 'restaurant-content-imgs';
+var allCaches = [
+  staticCacheName,
+  contentImgsCache
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'data/restaurants.json',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') &&
+            !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  var requestUrl = new URL(event.request.url);
+  console.log(requestUrl.pathname);
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname.startsWith('/img/')) {
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+function servePhoto(request) {
+  var storageUrl = request.url;
+
+  return caches.open(contentImgsCache).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
